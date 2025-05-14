7a6e9b770c93a25b00d11b448b33c1de745f04c8
Mario Mourao <mario.cmourao@decsis.pt>
2025-05-14 18:32:35 +0100

    Update test for root endpoint to expect a 500 status code

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,7 +15,7 @@ def clear_db():
 
 def test_read_root():
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == 500
     assert "message" in response.json()
     assert "Welcome" in response.json()["message"]
 
